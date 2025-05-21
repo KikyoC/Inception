0c19a91ec2be3a929051678bec82d56b171ba885
KikyoC <tom@swordfun.fr>
2025-05-21 12:15:20 +0200

    Removed log message

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -4,7 +4,7 @@ sed -i "s/"'$DOMAIN'"/$DOMAIN/g" /etc/nginx/http.d/default.conf
 
 if [ -e /etc/nginx/$DOMAIN.pem ]
 then
-	echo "here"
+	echo "Certificate already generated"
 else
 	cd /etc/nginx && mkcert $DOMAIN
 fi
