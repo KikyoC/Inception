52ef0fdff662d40ca690bd63c065b6fb6628bc86
Tom GAUTHIER <tom@swordfun.fr>
2025-06-09 13:47:56 +0200

    Adding default user

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,6 +5,8 @@ mv /usr/bin/php84 /usr/bin/php
 
 wp core install --url="http://$DOMAIN" --title="Joy" --admin_user="$WORDPRESS_USER" --admin_password="$WORDPRESS_PASS" --admin_email="$WORDPRESS_MAIL" --locale=en_US
 
+wp user create cmorel random@42.fr --role=subscriber --user_pass="$CMOREL"
+
 wp plugin install redis-cache --activate
 wp redis enable
 
